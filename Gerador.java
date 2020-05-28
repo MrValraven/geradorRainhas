353d9695a8ab0dba0ae6a85446456cac0344a07c
Tiago Costa <43713396+MrValraven@users.noreply.github.com>
2020-05-28 11:12:04 +0100

    Versão final do gerador Random e All

--- Gerador.java
+++ Gerador.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Gerador {
@@ -10,28 +13,45 @@ public class Gerador {
         int numberOfSize = 0;
         int dimensions = 0;
 
-        /*************************************************************************************************************************************** 
-         CONFIGURAÇÕES PARA VERSÃO FINAL - Para obter input diretamente da linha de comandos
-        if(args[0].equals("random")){
+        // Configuração final para aceitar argumentos do terminal
+        if(args.equals("random")){
             numberOfConfigs = Integer.parseInt(args[3]);
             numberOfQueens = Integer.parseInt(args[2]);
             numberOfSize = Integer.parseInt(args[1]);
             dimensions = numberOfSize * numberOfSize;
 
-            generateStrings(numberOfConfigs, dimensions, numberOfQueens);
+            random(numberOfConfigs, dimensions, numberOfQueens);
         }
-        else if(args[0].equals("all")){
+        else if(args.equals("all")){
             numberOfQueens = Integer.parseInt(args[1]);
-            dimensions = numberOfQueens * numberOfQueens;
-
-            generateAllStrings(dimensions, numberOfQueens); 
+            all(numberOfQueens); 
         }
-        *****************************************************************************************************************************************/
-
-         System.out.println("Digite o número de configurações: ");
-        numberOfConfigs = scanner.nextInt();
-        scanner.nextLine();
-        generateStrings(numberOfConfigs,64,8);   
+        else if(args.length == 0){
+            System.out.println("Please choose a mode: 'Random' or 'all' ");
+            String mode = scanner.nextLine();
+            if(mode.contains("Random")){
+                System.out.println("Digite o número de configurações: ");
+                numberOfConfigs = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Digite a dimensão do tabuleiro: ");
+                numberOfSize = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Digite o número de rainhas: ");
+                numberOfQueens = scanner.nextInt();
+                scanner.nextLine();
+                random(numberOfSize,numberOfQueens,numberOfConfigs);
+            }
+            else if(mode.contains("all")){
+                int value = 0;
+                System.out.println("Value: ");
+                value = scanner.nextInt();
+                all(value);
+            }
+            else{
+                System.out.println("Invalid mode, please try again");
+                mode = scanner.nextLine();
+            }
+        }
         scanner.close(); 
     }
 
@@ -45,7 +65,7 @@ public class Gerador {
          return dash;
     }
     
-    private static StringBuilder createStringWithRainhas(int size, int numberOfQueens){
+    private static String addQueensToString(int size, int numberOfQueens) {
         StringBuilder string = new StringBuilder(dashLine(size));
         int queenCounter = 0;
 
@@ -55,54 +75,63 @@ public class Gerador {
                     queenCounter++;
                 }
             }
-            System.out.println(string);
-            return string;
+            return string.toString();
     }
 
-    public static void generateStrings(int numberOfConfigs, int dimensions, int numberOfQueens){ 
-
-        for(int i = 0; i < numberOfConfigs; i++){
-            createStringWithRainhas(dimensions , numberOfQueens);
-        }
-    }
-
-    public static void generateAllStrings(int numberOfConfigs, int dimensions, int numberOfQueens){
-        // Combinaçoes =  nCr ou C(n,r) = n! / (n - r)!(r!)
-        /* If we include the first item, then we need to choose “r – 1″ elements from the remaining “n – 1″ items.
-        On the other hand, if we discard the first item, then we need to select “r” elements out of the remaining “n – 1″ items. */
-        // Em termos matemáticos : C(n,r) = C(n - 1, r - 1) + C(n - 1, r)
-   
-        String baseStringQueen = "D".repeat(numberOfQueens);
-        String baseStringNothing = "-".repeat(dimensions - numberOfQueens);
-        String stringFinal = baseStringQueen + baseStringNothing;
-        System.out.println(stringFinal);
-        System.out.println(stringFinal.length());
-        char[] stringArray = stringFinal.toCharArray();
-        
-        
-        for(int i = (numberOfQueens - 1); i < dimensions - 1 ; i++){
-            char aux1 = stringArray[i];
-            stringArray[i] = stringArray[i + 1];
-            stringArray[i + 1] = aux1;
-            System.out.println(new String(stringArray));   // Aqui DDDDDD-D
-            for(int j = 1 ; j < 8; j++){                   
-                char aux2 = stringArray[i - j];
-                stringArray[i - j] = stringArray[i - j + 1];
-                stringArray[i - j + 1] = aux2;
-                System.out.println(new String(stringArray)); // Cria um "pipeline" até ao final do primeiro ciclo for
+    public static List<String> random(int m, int q, int n){ 
+        List<String> configList = new ArrayList<String>();
+        int dimensions = m * m;
+        if(q <= m){
+            for(int i = 0; i < n; i++){
+                configList.add(addQueensToString(dimensions, q));
             }
         }
-
+        else{
+            System.out.println("Argumentos inválidos, o numero de rainhas (Q) é maior que a dimensão (M) permitida");
+        }
+        return configList;
     }
 
-
-
-
-
-
-
-
-
-
-
+    
+    public static List<String> all(int m){
+        List<String> allStrings = new ArrayList<String>();
+        int lowestValue = 0;
+        long highestValue = 0;
+        if(m < 2){
+            highestValue = (long)Math.pow(2, m);
+        }
+        else if(m == 2){
+            highestValue = (long)Math.pow(2, m+2);
+        }
+        else{
+            lowestValue = (int)Math.pow(2, m) - 1;
+            highestValue = (long)Math.pow(2, m * m);
+        }
+        int count = 0;
+        //Valores de M: 2 = 16 dá 6Combs | 3 = 511 dá 84Combs | 4 = 65535 dá 1820Combs | 5 = 33554431 dá 53130Combs | 6 = 68719476735 dá 1947792Combs 
+        String binary;
+        String finalString;
+        for(int i = 0; i < highestValue; i++){
+            binary = Integer.toBinaryString(i);
+            char[] binaryChar = binary.toCharArray();
+            count = 0;
+            for(int j = 0; j < binaryChar.length; j++) {
+                if(binaryChar[j] == '1'){
+                    count++;
+                    }
+            }
+            if(count == m){
+                if(binary.length() < m * m){
+                    binary = "-".repeat(m * m - binary.length()) + binary;
+                }
+                finalString = binary.replace('1', 'D');
+                finalString = finalString.replace('0', '-');
+                allStrings.add(finalString);
+            }
+        }
+        System.out.println("List size: " + allStrings.size());
+        System.out.println("List item 10: " + allStrings.get(10));
+        return allStrings;
+    }
 }
+
